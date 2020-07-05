a072b39d35b6ccb3bae46b5fa2e2c1432b951141
Kazumune Katagiri <kkaz123@gmail.com>
2020-07-05 13:58:18 +0900

    アノテーションプロセッサの設定値修正

--- src/main/java/micronaut/sample/infrastructure/doma/DaoConfig.java
+++ src/main/java/micronaut/sample/infrastructure/doma/DaoConfig.java
@@ -1,14 +1,9 @@
 package micronaut.sample.infrastructure.doma;
 
-import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.seasar.doma.AnnotateWith;
 import org.seasar.doma.Annotation;
 import org.seasar.doma.AnnotationTarget;
 
-@AnnotateWith(
-    annotations = {
-      @Annotation(target = AnnotationTarget.CLASS, type = Singleton.class),
-      @Annotation(target = AnnotationTarget.CONSTRUCTOR, type = Inject.class)
-    })
+@AnnotateWith(annotations = @Annotation(target = AnnotationTarget.CLASS, type = Singleton.class))
 public @interface DaoConfig {}
